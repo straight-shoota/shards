750d83b12e33d5563e3adaca13b49b1af89ee748
Johannes Müller <straightshoota@gmail.com>
2021-06-09 13:05:33 +0200

    Change manpage SOURCE_DATE_EPOCH to use source file change

--- docs.mk
+++ docs.mk
@@ -6,7 +6,6 @@ MAN_FILES := man/shards.1 man/shard.yml.5
 HTML_FILES := docs/shards.html docs/shard.yml.html
 
 SHARDS_VERSION := $(shell cat VERSION)
-SOURCE_DATE_EPOCH := $(shell (git show -s --format=%ct HEAD || stat -c "%Y" Makefile || stat -f "%m" Makefile) 2> /dev/null)
 
 docs: manpages
 
@@ -15,10 +14,10 @@ manpages: $(MAN_FILES)
 htmlpages: $(HTML_FILES)
 
 man/%.1 man/%.5: docs/%.adoc
-	SOURCE_DATE_EPOCH=$(SOURCE_DATE_EPOCH) $(ASCIIDOC) $(ASCIIDOC_OPTIONS) $< -b manpage -o $@
+	SOURCE_DATE_EPOCH=$(shell (git log -1 --format="%at" -- $< || stat -c "%Y" $<)) $(ASCIIDOC) $(ASCIIDOC_OPTIONS) $< -b manpage -o $@
 
 docs/%.html: docs/%.adoc
-	SOURCE_DATE_EPOCH=$(SOURCE_DATE_EPOCH) $(ASCIIDOC) $(ASCIIDOC_OPTIONS) $< -b html5 -o $@
+	SOURCE_DATE_EPOCH=$(shell (git log -1 --format="%at" -- $< || stat -c "%Y" $<)) $(ASCIIDOC) $(ASCIIDOC_OPTIONS) $< -b html5 -o $@
 
 clean_docs: phony
 	rm -f $(MAN_FILES)
